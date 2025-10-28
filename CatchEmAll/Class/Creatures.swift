94a9f36ce7d38ebc57ca957538415d25f7b51be9
DolaF003 <fakeye@bc.edu>
2025-10-28 16:19:31 -0400

    Catch em All Submission

--- CatchEmAll/Class/Creatures.swift
+++ CatchEmAll/Class/Creatures.swift
@@ -20,13 +20,16 @@ class Creatures {
     var urlString = "https://pokeapi.co/api/v2/pokemon/"
     var count = 0
     var creaturesArray: [Creature] = []
+    var isLoading = false
     
     func getData() async {
         print("🕸️ We are accessing the url \(urlString)")
+        isLoading = true
+
         //convert urlString to a special URL type
-        
         guard let url = URL(string: urlString) else {
             print("😡 JSON ERROR: Could not decode returned JSON data")
+            isLoading = false
             return
         }
         
@@ -36,15 +39,19 @@ class Creatures {
             //Try to decode JSON data into our own data structures
             guard let returned = try? JSONDecoder().decode(Returned.self, from: data) else {
                 print("😡 JSON Error: Could not decode returned JSON data")
+                isLoading = false
                 return
             }
             Task { @MainActor in
                 self.count = returned.count
                 self.urlString = returned.next ?? ""
                 self.creaturesArray = self.creaturesArray + returned.results
+                isLoading = false
             }
+            
         } catch {
             print("😡 JSON Error: Could not use URL at \(urlString) to get data and repsonse")
+            isLoading = false
         }
     }
 }
